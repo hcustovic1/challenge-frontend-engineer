57393dd8344eca568efafe4e85b9ad1d550df749
hcustovic1 <custovicharis@gmail.com>
2024-07-09 10:08:00 +0200

    fix: app background styles added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { OrderRoutes } from './routes/OrderRoutes';
+import { OrderProvider } from './context/OrderContext';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <Router>
-      <main>
-        <OrderRoutes />
-      </main>
-    </Router>
+    <OrderProvider>
+      <Router>
+        <main className={styles.appBackground}>
+          <OrderRoutes />
+        </main>
+      </Router>
+    </OrderProvider>
   );
 }
 
